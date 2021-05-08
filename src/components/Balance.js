4927ed868bbe574d2fc53c34751bcf9b8f40e658
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-08 11:12:44 -0500

    order the code

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -7,17 +7,17 @@ export const Balance = ({ transactions }) => {
 
   return (
     <section className = 'mb-4 container '>
-      <div className = 'row mb-2'>
+      <div className = 'mb-2'>
         <p className = 'mb-0 p-0'> Tu balance </p>
         <p className = 'fw-bold fs-4 p-0'>S/ {balance} </p>
       </div>
       <article className = 'row'>
-        <div className = 'col shadow-sm text-center bg-white me-1'>
-          <p>Ingresos</p>
+        <div className = 'col shadow-sm text-center bg-white me-1 '>
+          <p className = 'mt-1'>Ingresos</p>
           <p className = 'text-success'>S/ {totalIcome}</p>
         </div>
         <div className = 'col shadow-sm text-center bg-white'>
-          <p>Egresos</p>
+          <p className = 'mt-1'>Egresos</p>
           <p className = 'text-danger'>S/ {totalExpense}</p>
         </div>
       </article>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import  { ExpenseTrackerApp } from './ExpenseTrackerApp'
 
 import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <ExpenseTrackerApp/>,
